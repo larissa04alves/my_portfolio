a571b34ff740e44989ff42eb10bf91cfb750b80b
Larissa Alves <alves04.larissa@gmail.com>
2025-04-04 11:31:16 -0300

    feat: inicio header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Inter_Tight } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const intersans = Inter({
+  variable: "--font-inter-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const interLight = Inter_Tight({
+  variable: "--font-inter-light",
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${intersans.variable} ${interLight.variable} antialiased flex flex-col min-h-screen bg-background text-foreground font-sans mx-20 my-5`}
       >
         {children}
       </body>
